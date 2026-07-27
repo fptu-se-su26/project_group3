3ade6c98b4073786619e7b9fbdce34581ca7f46a
Nguyễn Đăng Phúc <ndangphuc-lgtm@github.com>
2026-07-27 15:16:39 +0700

    feat(F28): Manage student tuition - Calculate tuition from registered credits and price per credit, track due date and balance

---
+++ StudentManagement.WPF/Views/FinanceReportingView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace StudentManagement.WPF.Views
+{
+    public partial class FinanceReportingView : Window
+    {
+        public FinanceReportingView()
+        {
+            InitializeComponent();
+        }
+    }
+}
